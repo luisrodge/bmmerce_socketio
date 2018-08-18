662bb11ebfdd9efb7d9bf77a68a9f59d5918b6fd
Luis Rodriguez <luisrodge@outlook.com>
2018-08-17 21:29:55 -0600

    Add user rtyping event subscription and emitter

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ server.listen(6060, () => console.log('Example app listening on port: ', port));
 
 var connectedUsers = {};
 
+
 // Subscribe to redis events pushed by rails server
 var redis = require("redis").createClient();
 redis.subscribe("bmmerce:new-message"); // prefixed with rails app namespace
@@ -19,7 +20,9 @@ redis.on('message', function (channel, message) {
 
     var userScope = `${newMessage.engagement.listing_id}-${newMessage.recipient_id}`;
 
-    connectedUsers[userScope].emit(channel, newMessage);
+    if (connectedUsers[userScope]) {
+        connectedUsers[userScope].emit(channel, newMessage);
+    }
 
     // console.log('Redis channel: ', channel);
     // console.log('Redis message: ', message);
@@ -30,14 +33,23 @@ io.on('connection', function (socket) {
     console.log("A client has connected");
 
     socket.on('connectUser', function (userScope) {
-        console.log("USER SCOPE: ", userScope);
         //socket.join(room);
         connectedUsers[userScope] = socket;
-        console.log("CONNECTED USERS: ", connectedUsers)
-        console.log("CONNECTED USERS COUNT: ", Object.keys(connectedUsers).length)
+        //console.log("CONNECTED USERS: ", connectedUsers);
+        console.log("CONNECTED USERS COUNT: ", Object.keys(connectedUsers).length);
+    });
+
+    socket.on('senderTyping', function (data) {
+        console.log("Typing...");
+        console.log("Indicate to user: ", data)
+        if (connectedUsers[data.indicateScope]) {
+            connectedUsers[data.indicateScope].emit('onTyping', data.typing);
+        }
     });
 
     socket.on('disconnect', function () {
         console.log('Client disconnected');
+        //socket.io.reconnect();
+        //console.log("CONNECTED USERS COUNT: ", Object.keys(connectedUsers).length);
     });
 });
